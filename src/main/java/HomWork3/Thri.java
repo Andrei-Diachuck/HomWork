a560358bce6c3c39ad6fcfbc9ff11cbb899f6983
User <a>
2022-04-20 21:00:13 +0300

    hm3

--- src/main/java/HomWork3/Thri.java
+++ src/main/java/HomWork3/Thri.java
@@ -1,19 +1,15 @@
 package HomWork3;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
-public class First {
+public class Thri {
     public static void main(String[] args) {
-        int[] arr = {1, 1, 1, 0, 0, 0};
+        int[] arr = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) {
-                arr[i] = 1;
-            } else {
-                arr[i] = 0;
+            if (arr[i] < 6) {
+                arr[i] = arr[i] * 2;
             }
         }
         System.out.println(Arrays.toString(arr));
     }
 }
-
